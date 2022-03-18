62e48ffe7ec120044a05b412172690b8387f7941
nguyenkiendl <kienmap192@gmail.com>
2022-03-18 17:14:57 +0700

    reponsive phone media

--- js/main.js
+++ js/main.js
@@ -31,6 +31,40 @@ var $$ = document.querySelectorAll.bind(document);
         })
     });
 
+    // faqs control phone media
+    const tabcontrol = $$('.faqs__control');
+    const showClass = 'show';
+    const showTab = (t) => {
+         if( t.classList.contains(activeClass) ){
+            let cTab = t.dataset.tab;
+            t.classList.remove(activeClass);
+            $(cTab).classList.remove(showClass);
+        } else {
+            tabcontrol.forEach((el) => {
+            
+                    let idTab = el.dataset.tab;
+                    if(
+                        el.classList.contains(activeClass) && 
+                        $(idTab).classList.contains(showClass)
+                    ){
+                        el.classList.remove(activeClass);
+                        $(idTab).classList.remove(showClass);
+                    }
+                    let cTab = t.dataset.tab;
+                    t.classList.add(activeClass);
+                    $(cTab).classList.add(showClass);
+                
+            });
+        }
+    }
+
+    tabcontrol.forEach(t => {
+        t.addEventListener('click', function(e){
+            e.preventDefault();
+            showTab(this);
+        })
+    });
+
     // show/hide nav menu
 
 
